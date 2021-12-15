e604a88a79301a34351a57b070a0569c04feadc5
Giusande <91056214+Giusande@users.noreply.github.com>
2021-12-15 10:47:07 -0300

    Delete js directory

--- js/diario.js
+++
@@ -1,9 +0,0 @@
-var menutoggle = document.querySelector('.toggle');
-var nav = document.querySelector('#navbar');
-var overflow = document.querySelector('body');
-
-menutoggle.onclick = function(){
-  menutoggle.classList.toggle('active');
-  nav.classList.toggle('active1')
-  overflow.classList.toggle('active2')
-}
